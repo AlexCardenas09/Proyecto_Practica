af148392947dff501e741dd0bb60c51e4a4369b7
AlexCardenas09 <jacardenas951@misena.edu.co>
2022-10-31 19:11:51 -0500

    migraciones y llaves foraneas

---
+++ database/migrations/2022_10_26_231615_create_distribuidoras_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDistribuidorasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('distribuidoras', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombreProveedor');
+            $table->string('razonSocial');
+            $table->string('númeroContacto');
+            $table->string('ciudad');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('distribuidoras');
+    }
+}
---
+++ database/migrations/2022_10_26_232352_create_clientes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clientes', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->string('cedula');
+            $table->string('email');
+            $table->string('n°contacto');
+
+            $table->foreignId('prodctos_id')->constrained('productos');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clientes');
+    }
+}
